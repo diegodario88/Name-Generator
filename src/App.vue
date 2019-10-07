911c5a54c599cbffa732d7eed157e4e6d007fd11
Diego Dario <diegodario88@gmail.com>
2019-10-07 00:15:00 -0300

    added delete function and auto generate

--- src/App.vue
+++ src/App.vue
@@ -16,14 +16,32 @@
             <div class="card">
               <div class="card-body">
                 <ul class="list-group">
-                  <li
-                    class="list-group-item"
-                    v-for="prefixo in prefixos"
-                    v-bind:key="prefixo"
-                  >{{ prefixo }}</li>
+                  <li class="list-group-item" v-for="prefixo in prefixos" v-bind:key="prefixo">
+                    <div class="row">
+                      <div class="col-md">{{ prefixo }}</div>
+                      <div class="col-md text-right">
+                        <button class="btn btn-danger" v-on:click="deletarPrefixo(prefixo)">
+                          <span class="fa fa-trash"></span>
+                        </button>
+                      </div>
+                    </div>
+                  </li>
                 </ul>
                 <br />
-                <input class="form-control" type="text" placeholder="Digite o prefixo" />
+                <div class="input-group">
+                  <input
+                    class="form-control"
+                    type="text"
+                    v-model="prefixo"
+                    v-on:keyup.enter="addPrefixo(prefixo)"
+                    placeholder="Digite o prefixo"
+                  />
+                  <div class="input-group-append">
+                    <button class="btn btn-info" v-on:click="addPrefixo(prefixo)">
+                      <span class="fa fa-plus"></span>
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -35,14 +53,32 @@
             <div class="card">
               <div class="card-body">
                 <ul class="list-group">
-                  <li
-                    class="list-group-item"
-                    v-for="sufixo in sufixos"
-                    v-bind:key="sufixo"
-                  >{{ sufixo }}</li>
+                  <li class="list-group-item" v-for="sufixo in sufixos" v-bind:key="sufixo">
+                    <div class="row">
+                      <div class="col-md">{{ sufixo }}</div>
+                      <div class="col-md text-right">
+                        <button class="btn btn-danger" v-on:click="deletarSufixo(sufixo)">
+                          <span class="fa fa-trash"></span>
+                        </button>
+                      </div>
+                    </div>
+                  </li>
                 </ul>
                 <br />
-                <input class="form-control" type="text" placeholder="Digite o sufixo" />
+                <div class="input-group">
+                  <input
+                    class="form-control"
+                    type="text"
+                    v-model="sufixo"
+                    placeholder="Digite o sufixo"
+                    v-on:keyup.enter="addSufixo(sufixo)"
+                  />
+                  <div class="input-group-append">
+                    <button class="btn btn-info" v-on:click="addSufixo(sufixo)">
+                      <span class="fa fa-plus"></span>
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -50,12 +86,16 @@
         <br />
         <h5>
           Domínios
-          <span class="badge badge-info">{{ domains.length }}</span>
+          <span class="badge badge-info">{{ dominios.length }}</span>
         </h5>
         <div class="card">
           <div class="card-body">
             <ul class="list-group">
-              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">{{ domain }}</li>
+              <li
+                class="list-group-item"
+                v-for="dominio in dominios"
+                v-bind:key="dominio"
+              >{{ dominio }}</li>
             </ul>
           </div>
         </div>
@@ -67,14 +107,17 @@
 <script>
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import { log } from "util";
 
 export default {
   name: "app",
   data: function() {
     return {
+      prefixo: "",
+      sufixo: "",
       prefixos: ["Air", "Jet", "Flight"],
       sufixos: ["Hub", "Station", "Mart"],
-      domains: [
+      dominios: [
         "AirHub",
         "AirStation",
         "AirMart",
@@ -86,6 +129,35 @@ export default {
         "FlightMart"
       ]
     };
+  },
+
+  methods: {
+    addPrefixo(prefixo) {
+      this.prefixos.push(prefixo);
+      this.prefixo = "";
+      this.generate();
+    },
+    addSufixo(sufixo) {
+      this.sufixos.push(sufixo);
+      this.sufixo = "";
+      this.generate();
+    },
+    generate() {
+      this.dominios = [];
+      this.prefixos.forEach(prefix => {
+        this.sufixos.forEach(sufix => {
+          this.dominios.push(prefix + sufix);
+        });
+      });
+    },
+    deletarPrefixo(prefix) {
+      this.prefixos.splice(this.prefixos.indexOf(prefix), 1);
+      this.generate();
+    },
+    deletarSufixo(sufix) {
+      this.sufixos.splice(this.sufixos.indexOf(sufix), 1);
+      this.generate();
+    }
   }
 };
 </script>
